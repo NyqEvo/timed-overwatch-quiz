dd7a20f37da4fe0e097d15807d4e1a68333f1b37
NyqEvo <117671943+NyqEvo@users.noreply.github.com>
2022-11-30 21:14:54 -0500

    progress from second day of work

--- assets/script.js
+++ assets/script.js
@@ -4,76 +4,102 @@ var li2 = document.querySelector(".answerTwo");
 var li3 = document.querySelector(".answerThree");
 var li4 = document.querySelector(".answerFour");
 var timer = document.querySelector(".timer");
+var startButton = document.querySelector(".start");
 
 
 var questionOne = {
-    question: "????????",
-    answerOne: "wrong",
-    answerTwo: "correct",
-    answerThree: "wrong",
-    answerFour: "wrong",
-    correctAnswer: true
+    question: "why",
+    answerOne: ["wrong", false],
+    answerTwo: ["correct", true],
+    answerThree: ["wrong", false],
+    answerFour: ["wrong", false]
 }
 
 var questionTwo = {
-    question: "",
-    answerOne: "",
-    answerTwo: "",
-    answerThree: "",
-    answerFour: "",
-    correctAnswer: true
+    question: "stuff",
+    answerOne: ["wrong", false],
+    answerTwo: ["wrong", false],
+    answerThree: ["wrong", false],
+    answerFour: ["correct", true]
 }
 
 var questionThree = {
-    question: "",
-    answerOne: "",
-    answerTwo: "",
-    answerThree: "",
-    answerFour: "",
-    correctAnswer: true
+    question: "why tho",
+    answerOne: ["wrong", false],
+    answerTwo: ["correct", true],
+    answerThree: ["wrong", false],
+    answerFour: ["wrong", false]
 }
 
 var questionFour = {
-    question: "",
-    answerOne: "",
-    answerTwo: "",
-    answerThree: "",
-    answerFour: "",
-    correctAnswer: true
+    question: "also stuff",
+    answerOne: ["correct", true],
+    answerTwo: ["wrong", false],
+    answerThree: ["wrong", false],
+    answerFour: ["wrong", false]
 }
 
 var questionFive = {
-    question: "",
-    answerOne: "",
-    answerTwo: "",
-    answerThree: "",
-    answerFour: "",
-    correctAnswer: true
+    question: "thing",
+    answerOne: ["wrong", false],
+    answerTwo: ["correct", true],
+    answerThree: ["wrong", false],
+    answerFour: ["wrong", false]
 }
 
 var questionList = [questionOne, questionTwo, questionThree, questionFour, questionFive];
-var timeLeft = 75
-var currentQuestion = 0
-
-startQuiz.addEventListener("click", displayQuestion)
+var timeLeft = 75;
+var currentQuestion = 0;
+var correctAnswer = false;
 
 function startQuiz() {
-    var timeInterval = setInterval(function() {
+    var timeInterval = setInterval(function () {
         timeLeft--;
         timer.textContent = timeLeft + " seconds left";
-        displayQuestion();
         if (timeLeft === 0) {
             clearInterval(timeInterval);
+            //endQuiz()
         }
     }, 1000)
+    displayQuestion(currentQuestion);
 }
 
-function displayQuestion (num) {
-    currentQuestion += num
+function displayQuestion(num) {
+    //if (currentQuestion > 4) {}
     h1El.textContent = questionList[num].question
-    li1.textContent = questionList[num].answerOne
-    li2.textContent = questionList[num].answerTwo
-    li3.textContent = questionList[num].answerThree
-    li4.textContent = questionList[num].answerFour
+    li1.textContent = questionList[num].answerOne[0]
+    if (questionList[num].answerOne[1]){
+        li1.setAttribute("id","true")
+    }
+    li2.textContent = questionList[num].answerTwo[0]
+    if (questionList[num].answerTwo[1]){
+        li2.setAttribute("id","true")
+    }
+    li3.textContent = questionList[num].answerThree[0]
+    if (questionList[num].answerThree[1]){
+        li3.setAttribute("id","true")
+    }
+    li4.textContent = questionList[num].answerFour[0]
+    if (questionList[num].answerFour[1]){
+        li4.setAttribute("id","true")
+    }
+    currentQuestion++
 }
 
+function nextQuestion(answer) {
+    if (answer !== "true") {
+            timeLeft -= 15;
+            displayQuestion(currentQuestion);
+            //endQuiz()
+    } else {
+        var button = document.getElementById("true")
+        button.removeAttribute("id")
+        displayQuestion(currentQuestion);
+    }
+}
+
+startButton.addEventListener("click", startQuiz);
+// li1.addEventListener("click", nextQuestion);
+// li2.addEventListener("click", nextQuestion);
+// li3.addEventListener("click", nextQuestion);
+// li4.addEventListener("click", nextQuestion);
